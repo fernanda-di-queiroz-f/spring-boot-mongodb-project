5abc2a7627c81c36c45c3e502a429dafb151d6e5
fernanda-di-queiroz-f <fernanda1queiroz44@gmail.com>
2020-08-28 12:19:03 -0300

    First commit

---
+++ src/main/java/com/fpackage/sbmongodbproject/SbmongodbprojectApplication.java
@@ -0,0 +1,13 @@
+package com.fpackage.sbmongodbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmongodbprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmongodbprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fpackage/sbmongodbproject/SbmongodbprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpackage.sbmongodbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmongodbprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
