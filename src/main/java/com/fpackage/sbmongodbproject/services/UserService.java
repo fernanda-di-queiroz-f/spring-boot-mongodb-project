1d198e0ab4b20fcb35524f464f4e0f3721b277ff
fernanda-di-queiroz-f <fernanda1queiroz44@gmail.com>
2020-09-04 22:24:25 -0300

    findById and Exception Handler

--- src/main/java/com/fpackage/sbmongodbproject/services/UserService.java
+++ src/main/java/com/fpackage/sbmongodbproject/services/UserService.java
@@ -1,12 +1,14 @@
 package com.fpackage.sbmongodbproject.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fpackage.sbmongodbproject.domain.User;
 import com.fpackage.sbmongodbproject.repository.UserRepository;
+import com.fpackage.sbmongodbproject.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 
 }
---
+++ src/main/java/com/fpackage/sbmongodbproject/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.fpackage.sbmongodbproject.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
