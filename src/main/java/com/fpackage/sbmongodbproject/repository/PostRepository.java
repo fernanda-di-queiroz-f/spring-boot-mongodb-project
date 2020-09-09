bcf5d13521ff349cb550e3bd0f2bf8677a5e0b48
fernanda-di-queiroz-f <fernanda1queiroz44@gmail.com>
2020-09-09 11:57:51 -0300

    Post entity

---
+++ src/main/java/com/fpackage/sbmongodbproject/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.fpackage.sbmongodbproject.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fpackage.sbmongodbproject.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
