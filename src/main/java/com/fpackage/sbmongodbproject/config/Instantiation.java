dfa39352e91e103635045465627a0f1cd67d6bfd
fernanda-di-queiroz-f <fernanda1queiroz44@gmail.com>
2020-09-10 11:17:32 -0300

    AuthorDTO

--- src/main/java/com/fpackage/sbmongodbproject/config/Instantiation.java
+++ src/main/java/com/fpackage/sbmongodbproject/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.fpackage.sbmongodbproject.domain.Post;
 import com.fpackage.sbmongodbproject.domain.User;
+import com.fpackage.sbmongodbproject.dto.AuthorDTO;
 import com.fpackage.sbmongodbproject.repository.PostRepository;
 import com.fpackage.sbmongodbproject.repository.UserRepository;
 
@@ -29,17 +30,19 @@ public class Instantiation implements CommandLineRunner {
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-		Post post1 = new Post(null, sdf.parse("21/03/2019"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
-				maria);
-		Post post2 = new Post(null, sdf.parse("21/03/2019"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
-				maria);
-
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2019"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
+				new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("21/03/2019"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
+				new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
