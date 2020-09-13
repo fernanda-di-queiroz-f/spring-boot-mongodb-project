2d7d3a4018157034687b7293938df5a536ebf8b0
fernanda-di-queiroz-f <fernanda1queiroz44@gmail.com>
2020-09-13 20:58:43 -0300

    Get posts by id

---
+++ src/main/java/com/fpackage/sbmongodbproject/services/PostService.java
@@ -0,0 +1,23 @@
+package com.fpackage.sbmongodbproject.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fpackage.sbmongodbproject.domain.Post;
+import com.fpackage.sbmongodbproject.repository.PostRepository;
+import com.fpackage.sbmongodbproject.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+
+}
